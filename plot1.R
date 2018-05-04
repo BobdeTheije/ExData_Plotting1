e9e24518d629cc64f51fb9f6a2b75fc3dbef1f75
BobdeTheije <bob@theije.com>
2018-05-04 10:05:54 +0200

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,37 @@
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+
+#Download if not already available
+if(!file.exists("./household_power_consumption.zip"))
+{
+  download.file(fileUrl,destfile="./household_power_consumption.zip")
+}
+
+
+# Unzip if not already available
+if (!file.exists("household_power_consumption.txt")) 
+{ 
+ unzip("./household_power_consumption.zip") 
+}
+
+# read the data
+hhpowercon_all <-read.csv2 ("./household_power_consumption.txt", na.strings = "?", dec=".")
+
+#subset the data needed
+hhpowercon <- subset(hhpowercon_all, Date == "1/2/2007"|Date =="2/2/2007")
+
+# other option only read the subset
+# df <- read.csv.sql("./household_power_consumption.txt", "select * from file where Date = '1/2/2007' or Date = '2/2/2007' ", sep=";", na.strings = "?")
+# dbDisconnect()
+
+
+# convert Date column to date
+hhpowercon$Date <- as.Date(hhpowercon$Date, "%d/%m/%Y") 
+
+# Create png file
+
+png(file = "plot1.png", width = 480, height = 480)
+
+hist(hhpowercon$Global_active_power, main = "Global Active Power", col = "red", xlab = "Global Active Power (kilowatts)")
+
+dev.off()
